7d2d5a0fbf859942f69bce8bdeb613be65f1bb00
just13just <bkryzhniy@gmail.com>
2021-03-09 01:22:14 +0200

    fix: misprint in markup

--- src/App.js
+++ src/App.js
@@ -18,42 +18,42 @@ const App = () => {
   return (
     <main className="main-container">
       <div className='img-list'>
-        <dib className='img-container'>
+        <div className='img-container'>
           <img className='img' src={img_1} alt='img 1' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_2} alt='img 2' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_3} alt='img 3' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_4} alt='img 4' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_5} alt='img 5' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_6} alt='img 6' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_7} alt='img 7' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_8} alt='img 8' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_9} alt='img 9' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_10} alt='img 10' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_11} alt='img 11' />
-        </dib>
-        <dib className='img-container'>
+        </div>
+        <div className='img-container'>
           <img className='img' src={img_12} alt='img 12' />
-        </dib>
+        </div>
       </div>
     </main>
   );
