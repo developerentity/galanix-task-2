1ff27f4b7e10bf9bf4256cf427ca89fc0002f8b2
just13just <bkryzhniy@gmail.com>
2021-03-15 15:12:06 +0200

    refactor: combine pictures in one array

--- src/App.js
+++ src/App.js
@@ -1,59 +1,21 @@
 import './App.css'
-import img_1 from './img/1.jpg'
-import img_2 from './img/2.jpg'
-import img_3 from './img/3.jpg'
-import img_4 from './img/4.jpg'
-import img_5 from './img/5.jpg'
-import img_6 from './img/6.jpg'
-import img_7 from './img/7.jpg'
-import img_8 from './img/8.jpg'
-import img_9 from './img/9.jpg'
-import img_10 from './img/10.jpg'
-import img_11 from './img/11.jpg'
-import img_12 from './img/12.jpg'
+import { imagesArr } from './imagesCombiner';
 
 
 const App = () => {
 
+  const imagesList = imagesArr.map(img => {
+    return (
+      <div className='img-container'>
+        <img className='img' src={img.src} alt={`img ${img.id}`} />
+      </div>
+    )
+  })
+
   return (
     <main className="main-container">
       <div className='img-list'>
-        <div className='img-container'>
-          <img className='img' src={img_1} alt='img 1' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_2} alt='img 2' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_3} alt='img 3' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_4} alt='img 4' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_5} alt='img 5' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_6} alt='img 6' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_7} alt='img 7' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_8} alt='img 8' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_9} alt='img 9' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_10} alt='img 10' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_11} alt='img 11' />
-        </div>
-        <div className='img-container'>
-          <img className='img' src={img_12} alt='img 12' />
-        </div>
+        {imagesList}
       </div>
     </main>
   );
---
+++ src/imagesCombiner.js
@@ -0,0 +1,19 @@
+import img_1 from './img/1.jpg'
+import img_2 from './img/2.jpg'
+import img_3 from './img/3.jpg'
+import img_4 from './img/4.jpg'
+import img_5 from './img/5.jpg'
+import img_6 from './img/6.jpg'
+import img_7 from './img/7.jpg'
+import img_8 from './img/8.jpg'
+import img_9 from './img/9.jpg'
+import img_10 from './img/10.jpg'
+import img_11 from './img/11.jpg'
+import img_12 from './img/12.jpg'
+
+export const imagesArr = [
+    { src: img_1, id: 1 }, { src: img_2, id: 2 }, { src: img_3, id: 3 },
+    { src: img_4, id: 4 }, { src: img_5, id: 5 }, { src: img_6, id: 6 },
+    { src: img_7, id: 7 }, { src: img_8, id: 8 }, { src: img_9, id: 9 },
+    { src: img_10, id: 10 }, { src: img_11, id: 11 }, { src: img_12, id: 12 },
+]
